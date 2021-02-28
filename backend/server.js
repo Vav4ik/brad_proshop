dd9be7c28bd946ea143ec778d87f74e6afc535f6
Valdis <wawaysms@gmail.com>
2021-02-28 16:54:09 +0000

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,18 @@ app.get("/api/config/paypal", (req, res) => {
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
+//to serve React files in frontend - compliled and static (on Heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+
 //error handling middleware
 app.use(notFound);
 app.use(errorHandler);
